b7383f74c410d9b99bee5cce50dccd98fea7a7c4
Caroline <49812539+CarolKogei@users.noreply.github.com>
2025-01-15 17:01:33 +0300

    trouble shooting .yaml file to add sql volume to read .sql script

--- dags/init_db.py
+++ dags/init_db.py
@@ -1,21 +1,3 @@
-# from airflow import DAG
-# from airflow.providers.postgres.operators.postgres import PostgresOperator
-# from datetime import datetime
-
-# with DAG(
-#     dag_id='initialize_postgres_db',
-#     start_date=datetime(2024, 1, 1),
-#     schedule_interval=None,  # Run manually or only once
-#     catchup=False,
-# ) as dag:
-
-#     init_db = PostgresOperator(
-#         task_id='create_tables',
-#         postgres_conn_id='postgres_default',
-#         sql= r'\opt\airflow\sql\init.sql',
-#         dag=dag,
-#     )
-
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime
